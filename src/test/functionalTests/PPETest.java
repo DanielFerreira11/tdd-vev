ddf1e16b6111b6d217565503af9bac119fca243c
DanielFerreira11 <daniel.familiaferreira@gmail.com>
2024-03-20 23:52:13 -0300

    Adiciona Suite de Testes funcionais Sistema 1

---
+++ src/test/functionalTests/PPETest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ReservaTest {
+
+    @Test
+    public void testReservaValida_DestinoOrigemDiferentesDataValida() {
+        Reserva reserva = new Reserva("São Paulo", "Rio de Janeiro", "2024-04-01", 2);
+        assertTrue(reserva.ehReservaValida());
+    }
+
+    @Test
+    public void testReservaInvalida_DestinoOrigemVaziosDataValidaNumPassageirosInvalido() {
+        Reserva reserva = new Reserva("", "", "2023-03-01", 0);
+        assertFalse(reserva.ehReservaValida());
+    }
+
+    @Test
+    public void testCadastroValido_NomeNumContatoPassageiroValido() {
+        Passageiro passageiro = new Passageiro("João Silva", "1", "joão@email.com");
+        assertTrue(passageiro.ehCadastroValido());
+    }
+
+    @Test
+    public void testCadastroInvalido_NomeNumPassageiroVaziosContatoValido() {
+        Passageiro passageiro = new Passageiro("", "", "joão@email.com");
+        assertFalse(passageiro.ehCadastroValido());
+    }
+}
