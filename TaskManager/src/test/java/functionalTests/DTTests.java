4c3892850df2e26a68794936ee0a3d0175dce07b
João Vitor <joao.luciano@lsd.ufcg.edu.br>
2024-03-20 23:47:17 -0300

    gerenciador de tarefas (tabela de decisao)

--- TaskManager/src/test/java/functionalTests/DTTests.java
+++ TaskManager/src/test/java/functionalTests/DTTests.java
@@ -12,12 +12,72 @@ class DTTests {
 
     @SuppressWarnings("deprecation")
     @Test
-    void testHighAndMediumPriorityTasksWithSameDueDate() {
+    void testCreateTask() {
     	Task tarefa = new Task("Tarefa 1", "Descrição da Tarefa 1", new Date(2024, 3, 25), Priority.HIGH);
-    	manager.addTask(tarefa);
-        
     	assertFalse(manager.getTasks().contains(tarefa));
+    	
+    	manager.addTask(tarefa);
+    	
     	assertEquals(1, manager.getTasks().size());
+    	assertEquals(tarefa, manager.getTasks().get(0));
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Test
+    void testTaskWithInvalidPriority() {
+    	Task tarefa = new Task("Tarefa 1", "Descrição da Tarefa 1", new Date(2024, 3, 25), "");
+    	assertFalse(manager.getTasks().contains(tarefa));
+    	
+    	manager.addTask(tarefa);
+    	
+    	assertEquals(0, manager.getTasks().size());
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Test
+    void testTaskWithInvalidDueDate() {
+    	Task tarefa = new Task("Tarefa 1", "Descrição da Tarefa 1", "", Priority.HIGH);
+    	assertFalse(manager.getTasks().contains(tarefa));
+    	
+    	manager.addTask(tarefa);
+    	
+    	assertEquals(0, manager.getTasks().size());
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Test
+    void testTaskWithInvalidIndex() {
+    	Task tarefa = new Task("Tarefa 1", "Descrição da Tarefa 1", new Date(2024, 3, 25), Priority.HIGH);
+    	assertFalse(manager.getTasks().contains(tarefa));
+    	
+    	manager.addTask(tarefa);
+    	
+    	manager.removeTask(2);
+    	assertEquals(tarefa, manager.getTasks().get(0));
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Test
+    void testRemoveTask() {
+    	Task tarefa = new Task("Tarefa 1", "Descrição da Tarefa 1", new Date(2024, 3, 25), Priority.HIGH);
+    	assertFalse(manager.getTasks().contains(tarefa));
+    	
+    	manager.addTask(tarefa);
+    	
+    	manager.removeTask(0);
+    	assertEquals(0, manager.getTasks().size());
+    }
+    
+    @SuppressWarnings("deprecation")
+    @Test
+    void testUpdateTask() {
+    	Task tarefa = new Task("Tarefa 1", "Descrição da Tarefa 1", new Date(2024, 3, 25), Priority.HIGH);
+    	assertFalse(manager.getTasks().contains(tarefa));
+    	
+    	manager.addTask(tarefa);
+    	tarefa.setDescription("Lavar o carro");
+    	manager.updateTask(0, tarefa);
+    	
     	assertEquals(tarefa, manager.getTasks().get(0));
     }
 }
